bdeec7b718c66c8ff4a7c7d2e630edde48ee18b9
Кирилл Михайлов <137108844+KirillMikhailov442@users.noreply.github.com>
2025-07-29 04:19:51 +0500

    fix: laoding changed

--- src/screens/Loading/Loading.tsx
+++ src/screens/Loading/Loading.tsx
@@ -1,13 +1,13 @@
 import { NextPage } from "next";
 import logo from '@/../public/icons/192x192.png'
+import progress from '@images/loading.gif';
 import Image from "next/image";
-import { CircularProgress } from "@chakra-ui/react";
 import styles from './Loading.module.scss';
 
 const LoadingScreen: NextPage = () =>{
     return<div className={styles.page}>
         <Image height={100} width={100} src={logo} alt="logo" loading="lazy"/>
-        <CircularProgress isIndeterminate color='blue.500' />
+        <Image src={progress} alt="progress"/>
     </div>
 }
 
